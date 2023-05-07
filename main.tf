9e5e44d5e85db6a5acfb73ed49e30ba8e36d61cd
Arumugam Veeman <arumugam.veeman@gmail.com>
2023-05-07 17:38:26 +0200

    test

--- main.tf
+++ main.tf
@@ -3,13 +3,24 @@ variable "client_id1" {}
 variable "client_secret1" {}
 variable "tenant_id1" {}
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-    }
-  }
-  required_version = ">= 1.3.9"
+resource "null_resource" "install_azure_cli" {
+  provisioner "local-exec" {
+    command = <<EOH
+
+      echo "Installing AzureCLI"
+      apt-get update
+            # install requirements
+      apt-get install -y curl apt-transport-https lsb-release gnupg jq
+            # add Microsoft as a trusted source
+      curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+      AZ_REPO=$(lsb_release -cs)
+      echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+      apt-get update
+      apt-get install azure-cli
+
+      EOH
+
+    interpreter = ["/bin/bash", "-c"]
 }
 
 provider "azurerm" {
