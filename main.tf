08b1a77c9629e000079f737f5c51ab4763d5da67
Arumugam Veeman <arumugam.veeman@gmail.com>
2023-05-07 17:35:19 +0200

    test

--- main.tf
+++ main.tf
@@ -3,6 +3,17 @@ variable "client_id1" {}
 variable "client_secret1" {}
 variable "tenant_id1" {}
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.46.0"
+    }
+  }
+
+  required_version = ">= 1.3.9"
+}
+
 provider "azurerm" {
   subscription_id = "${var.subscription_id1}"
   client_id       = "${var.client_id1}"
